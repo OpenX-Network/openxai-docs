673ca931bda64a475816be46aa22832cd9be65cf
John Forfar <john.forfar@gmail.com>
2026-07-12 12:03:25 +0800

    Set OpenX Network logo icon as site favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -14,6 +14,8 @@ const config: DocsThemeConfig = {
       <meta property="og:title" content="OpenX Network Docs" />
       <meta property="og:description" content="OpenX Network Documentation" />
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     </>
   ),
   sidebar: {
